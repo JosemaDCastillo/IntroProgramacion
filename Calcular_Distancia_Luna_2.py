dc8c156e4c146d0fada964adc7abacac8a8874d8
Josema <josemadlcastillot@gmail.com>
2020-09-18 17:11:32 -0400

    Anadir primer version calculadora Dustancia Luna 2

---
+++ Calcular_Distancia_Luna_2.py
@@ -0,0 +1,4 @@
+distancia_tierra_luna = 384400
+distancia_recorrida = int(input("Porfavor Ingrese distancia : "))
+distancia_restante = distancia_tierra_luna - distancia_recorrida
+print("distancia_restante):", (distancia_restante))
